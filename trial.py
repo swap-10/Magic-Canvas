1b1c767afe437fb4e275c907cd4f5ffd93864a66
swap-10 <swapnalv10@gmail.com>
2022-11-29 14:18:14 +0530

    Add whiteboard features Now opens a new whiteboard window and draws on that Still has a seperate window for video feed Add a cursor which indicates position on whiteboard even when pen-up Now performs smoothing on pointer locations to reduce the effect of noise

--- trial.py
+++ trial.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import mediapipe as mp
 
 mp_drawing = mp.solutions.drawing_utils
@@ -22,6 +23,9 @@ with mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5
     ) as hands:
+    whiteboard = np.full(shape=(800, 1400, 3), fill_value=255).astype(np.uint8)
+    pointer = 0
+    pointers = []
     while cap.isOpened():
         success, img = cap.read()
         if not success:
@@ -31,7 +35,7 @@ with mp_hands.Hands(
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         h, w, c = img.shape
         results = hands.process(img)
-
+        
         # Draw annotations
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         if results.multi_hand_landmarks:
@@ -45,22 +49,39 @@ with mp_hands.Hands(
                 )
                 
                 landmarks = [p for p in hand_landmarks.landmark]
+                pointer = landmarks[8]
+                pointers.append(pointer)
+                k_mov_avg = 5
+                if len(pointers) > k_mov_avg:
+                    pointers = pointers[-k_mov_avg:]
+                if len(pointers) == k_mov_avg:
+                    pointer.x = sum([p.x for p in pointers]) / len(pointers)
+                    pointer.y = sum([p.y for p in pointers]) / len(pointers)
                 if (distance(landmarks[5], landmarks[0]) < 2 * distance(landmarks[12], landmarks[9])):
                     if len(current) > 0:
                         history.append(current)
                     current = []
                     pass
                 else:
-                    pointer = [p for p in hand_landmarks.landmark][8]
                     current.append(pointer)
         
+        w = 1400
+        h = 800
+        '''
         for trail in history:
             for i in range(len(trail)-1):
-                cv2.line(img,(int(w*trail[i].x), int(h*trail[i].y)), (int(w*trail[i+1].x), int(h*trail[i+1].y)), (255, 0, 0), 5)
+                cv2.line(whiteboard,(int(w*trail[i].x), int(h*trail[i].y)), (int(w*trail[i+1].x), int(h*trail[i+1].y)), (255, 0, 0), 5)
+        '''
         for i in range(len(current)-1):
-            cv2.line(img,(int(w*current[i].x), int(h*current[i].y)), (int(w*current[i+1].x), int(h*current[i+1].y)), (255, 0, 0), 5)
+            cv2.line(whiteboard,(int(w*current[i].x), int(h*current[i].y)), (int(w*current[i+1].x), int(h*current[i+1].y)), (255, 0, 0), 5)
+
+        whiteboard_cur = whiteboard.copy()
+        if results.multi_hand_landmarks:
+            cv2.circle(whiteboard_cur, (int(w*pointer.x), int(h*pointer.y)), 5, (0, 255, 0), 3)
+            cv2.circle(whiteboard_cur, (int(w*pointer.x), int(h*pointer.y)), 1, (0, 255, 0), -1)
 
         cv2.imshow('Mediapipe Hands', cv2.flip(img, 1))
+        cv2.imshow("Whiteboard", cv2.flip(whiteboard_cur, 1))
 
         if cv2.waitKey(5) & 0xFF == 27:
                 break
